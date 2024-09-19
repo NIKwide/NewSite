7999ca4fd37e3e49be77439f902ba3ff82b5922c
NIKwide <123722691+NIKwide@users.noreply.github.com>
2024-09-19 13:26:37 +0300

    Add files via upload

---
+++ project/scripts/js/script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById('menu-toggle').addEventListener('click', function() {
+        var navMenu = document.getElementById('nav-menu');
+        navMenu.classList.toggle('active');
+    });
+});
